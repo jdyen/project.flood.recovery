4902652e223527aeeccf977459a06020615a930c
Adrian M Kitchingman (DEECA) <adrian.kitchingman@delwp.vic.gov.au>
2023-05-31 12:01:59 +1000

    newstuff

--- data_analysis.R
+++ data_analysis.R
@@ -19,6 +19,7 @@ library(corrplot)
 library(Hmisc)
 library(lmerTest)
 library(rstanarm)
+library(ggeffects)
 
 
 #read in data as dataframe 
@@ -84,15 +85,19 @@ flood_model_lmer_aov
 ##TAKES TIME TO RUN
 
 flood_model_stanlmer_jian <- stan_lmer(
-  log_cpue_p1 ~ before_after * hypoxia_rank * scientific_name +
+  log_cpue_p1 ~ before_after * hypoxia_rank +
+    (before_after * hypoxia_rank | scientific_name) +
     discharge_std +
     (1 | waterbody) + (1 | site_name),
   data = flood_data_ba)
 
 summary(flood_model_stanlmer_jian)
 
-plot(flood_model_stanlmer_jian, regex_pars = "before:hypoxia")
+plot(flood_model_stanlmer_jian, regex_pars = "after:hypoxia")
 
+
+dat <- ggpredict(flood_model_stanlmer_jian, terms = c("before_after", "hypoxia_rank", "scientific_name"))
+plot(dat, facet = TRUE, add.data = TRUE, connect.lines = TRUE)
 
 
 
